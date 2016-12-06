b4528227fb0edaf48084c84187b675f3d7d7b64b
Developer1 <Developer1@TA7>
2016-12-06 10:26:56 +0530

    PROT-7014 - Signing in Progress is spelt wrong - Implemented dialog box for signing in progress

--- Anglian/Anglian/Views/LoginPage.xaml.cs
+++ Anglian/Anglian/Views/LoginPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Anglian.Classes;
 using Anglian.Service;
 using Xamarin.Forms;
+using Acr.UserDialogs;
 
 namespace Anglian.Views
 {
@@ -31,6 +32,8 @@ namespace Anglian.Views
                 Password.Focus();
                 return;
             }
+            //PROT-7014
+            UserDialogs.Instance.ShowLoading("Signing In...", MaskType.Black);
             btnLogin.IsEnabled = false;
             LogonResult result = await DependencyService.Get<ILogon>().LogonAsync(
                 UserName.Text.Trim(),
@@ -40,14 +43,15 @@ namespace Anglian.Views
             if (result.InvalidDetails == true)
             {
                 await DisplayAlert("Warning", "Invalid Username and Password.", "OK");
+                UserDialogs.Instance.HideLoading();
                 UserName.Text = "";
                 Password.Text = "";
                 UserName.Focus();
-
             }
             else
             {
                 Session.Token = result.Token;
+                UserDialogs.Instance.HideLoading();
                 Session.CurrentUserName = UserName.Text.Trim();
                 Session.LoggedTime = DateTime.Now;
                 Device.BeginInvokeOnMainThread(() => Navigation.PushAsync(new MainMenuPage()));
